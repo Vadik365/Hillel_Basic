b90a78a8315502e95b4527a52d3ba7da4ff4b1cc
Vadik365 <vadiksoldatenko@gmail.com>
2026-03-03 22:34:49 +0200

    update lesson 2

--- main.py
+++ main.py
@@ -1,11 +1,46 @@
+#1
 # number = int(input("Enter a number: "))
 #
 # result = number ** 2
-# print(result)
+#print(result)
 
+#2
 # number1 = int(input("Enter a first number: "))
 # number2 = int(input("Enter a second number: "))
 # number3 = int(input("Enter a third number: "))
 #
 # result = (number1 + number2 + number3) / 3
 # print(result)
+
+#3
+# minutes = int(input("Enter minutes: "))
+# hours, minute = divmod(minutes, 60)
+# time = (f"{hours} h {minute} m")
+# print(time)
+
+#4
+# price_items = float(input("Price:"))
+# discount = int(input("Discount:"))
+#
+# final_price = price_items - (price_items * discount / 100)
+# print(f"To be paid: {final_price}$")
+
+#5
+# number = int(input("Enter the number:"))
+# last_num = number % 10
+# print(last_num)
+
+#6
+# lenght = int(input("Lenght:"))
+# width = int(input("Width:"))
+# area = (lenght + width) * 2
+# print(area)
+
+#7
+# number = int(input("Enter a four number: "))
+# num1 = number // 1000
+# num2 = (number // 100) % 10
+# num3 = (number // 10) % 10
+# num4 = number % 10
+#
+# print(num1, num2, num3, num4, sep='\n')
