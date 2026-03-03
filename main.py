e69a23dd66951054b3f8e6fe6efff4eb77a0a025
Vadik365 <vadiksoldatenko@gmail.com>
2026-03-03 20:58:54 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+# number = int(input("Enter a number: "))
+#
+# result = number ** 2
+# print(result)
+
+# number1 = int(input("Enter a first number: "))
+# number2 = int(input("Enter a second number: "))
+# number3 = int(input("Enter a third number: "))
+#
+# result = (number1 + number2 + number3) / 3
+# print(result)
+
