47fa0cf1c4d6aac15db928339183c1ae9a8cd201
Vadik365 <vadiksoldatenko@gmail.com>
2026-03-06 21:55:00 +0200

    update lesson 3

---
+++ homework(3).py
@@ -0,0 +1,33 @@
+######## 3.1 ##########
+# num1 = int(input("Enter number #1:"))
+# operation = input("Enter operation (+, -, *, /):")
+# num2 = int(input("Enter number #2:"))
+#
+# match operation:
+#     case "+":
+#         print(num1 + num2)
+#     case "-":
+#         print(num1 - num2)
+#     case "*":
+#         print(num1 * num2)
+#     case "/":
+#         if num2 != 0:
+#             print(num1 / num2)
+#         else:
+#             print("Cannot divide by zero!")
+#     case _:
+#         print("Error: invalid operation!")
+
+######## 3.2 ##########
+# num_list = [3, 7, 9, 11, 15]
+# if len(num_list) > 1:
+#     num_list = [num_list[-1]] + num_list[:-1]
+# print(num_list)
+
+######## 3.3 ##########
+# my_list = [1, 2, 3, 4, 5, 6]
+# mid_list = (len(my_list) + 1) // 2
+# lists = [my_list [:mid_list], my_list [mid_list:]]
+# print(lists)
+
+
