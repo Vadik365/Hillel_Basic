60cfd2dd02e261f7df900f27e309ddec5a36dfe8
Vadik365 <vadiksoldatenko@gmail.com>
2026-03-20 22:41:30 +0200

    update lesson 7

--- homework_7.py
+++ homework_7.py
@@ -18,15 +18,10 @@ def correct_sentence(text):
     return text
 
 assert correct_sentence("greetings, friends") == "Greetings, friends.", 'Test1'
-print(correct_sentence("greetings, friends"))
 assert correct_sentence("hello") == "Hello.", 'Test2'
-print(correct_sentence("hello"))
 assert correct_sentence("Greetings. Friends") == "Greetings. Friends.", 'Test3'
-print(correct_sentence("Greetings, Friends"))
 assert correct_sentence("Greetings, friends.") == "Greetings, friends.", 'Test4'
-print(correct_sentence("Greetings, friends."))
 assert correct_sentence("greetings, friends.") == "Greetings, friends.", 'Test5'
-print(correct_sentence("Greetings, friends."))
 print("Оk")
 
 #### 7.3 Пошук підрядка ####
