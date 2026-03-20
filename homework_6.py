d75307806b276ed0c9a61060f7aaafd602edc042
Vadik365 <vadiksoldatenko@gmail.com>
2026-03-20 19:30:55 +0200

    update lesson 6

--- homework_6.py
+++ homework_6.py
@@ -26,36 +26,36 @@ while True:
     if start_index <= end_index:
         print(all_letters[start_index:end_index + 1])
     else:
-        print(all_letters[start_index:end_index - 1:-1])
+        print(all_letters[start_index::-1])
     break
 
 #### 6.2 Конвертер із числа в дату ###
-seconds = int(input("Enter seconds: "))
-days, remainder = divmod(seconds, 86400)
-hours, remainder = divmod(remainder, 3600)
-minutes, seconds = divmod(remainder, 60)
-
-if days % 10 == 1 and days % 100 != 11:
-    day_word = "день"
-elif days % 10 in [2, 3, 4] and days % 100 not in [12, 13, 14]:
-    day_word = "дні"
-else:
-    day_word = "днів"
-print(f"{days} {day_word}, {str(hours).zfill(2)}:{str(minutes).zfill(2)}:{str(seconds).zfill(2)}")
-
-#### 6.3 Добуток чисел ###
-number = input("Enter number: ")
-while  not number.isdigit():
-    print("Please enter number")
-    number = input("Enter number: ")
-number = int(number)
-while number > 9:
-    result = 1
-
-    for digit in str(number):
-        result *= int(digit)
-    number = result
-
-print(number)
+# seconds = int(input("Enter seconds: "))
+# days, remainder = divmod(seconds, 86400)
+# hours, remainder = divmod(remainder, 3600)
+# minutes, seconds = divmod(remainder, 60)
+#
+# if days % 10 == 1 and days % 100 != 11:
+#     day_word = "день"
+# elif days % 10 in [2, 3, 4] and days % 100 not in [12, 13, 14]:
+#     day_word = "дні"
+# else:
+#     day_word = "днів"
+# print(f"{days} {day_word}, {str(hours).zfill(2)}:{str(minutes).zfill(2)}:{str(seconds).zfill(2)}")
+#
+# #### 6.3 Добуток чисел ###
+# number = input("Enter number: ")
+# while  not number.isdigit():
+#     print("Please enter number")
+#     number = input("Enter number: ")
+# number = int(number)
+# while number > 9:
+#     result = 1
+#
+#     for digit in str(number):
+#         result *= int(digit)
+#     number = result
+#
+# print(number)
 
 
